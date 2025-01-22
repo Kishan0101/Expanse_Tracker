9131d0fac9f949616ef1bb782d89c96ce7b09ff6
Kishan Kumar Prajapati <98070544+Kishan0101@users.noreply.github.com>
2025-01-23 01:03:21 +0530

    Update axiosClient.js

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export const axiosClient = axios.create({
-    baseURL : 'https://expensify-tracker.onrender.com'
+    baseURL : 'https://expanse-tracker-rlds.onrender.com'
 })
